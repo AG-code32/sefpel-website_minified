9afc4eb49cfc5bd95f432d725c1e0e7fe8edcb7e
AG-code32 <i.gerson@hotmail.com>
2023-03-03 21:14:25 -0500

    nav and swiper

---
+++ javascript/main.js
@@ -0,0 +1,43 @@
+const boxServiceList = document.querySelector('.box__service-list');
+const serviceList = document.querySelector('.services-list');
+
+const navItems = document.querySelector('#nav__items')
+const openNavBtn = document.querySelector('#open__nav-btn')
+const closeNavBtn = document.querySelector('#close__nav-btn')
+
+// const mediaqueryList1440 = window.matchMedia("(max-width: 1440px)");
+// const mediaqueryList920 = window.matchMedia("(max-width: 920px)");
+
+boxServiceList.addEventListener('mouseover', () => {
+    serviceList.style.display = 'flex';
+})
+
+boxServiceList.addEventListener('mouseout', () => {
+    serviceList.style.display = 'none';
+})
+
+
+
+openNavBtn.addEventListener('click', () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'flex';
+})
+
+closeNavBtn.addEventListener('click', () => {
+    closeNavBtn.style.display = 'none';
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'flex';
+})
+
+// if (mediaqueryList1440.matches) {
+//     navItems.style.display = 'flex';
+//     closeNavBtn.style.display = 'none';
+//     openNavBtn.style.display = 'none';
+// }
+
+// if (mediaqueryList920.matches) {
+//     navItems.style.display = 'none';
+//     closeNavBtn.style.display = 'none';
+//     openNavBtn.style.display = 'flex';
+// }
